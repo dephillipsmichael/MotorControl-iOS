e5a69c6a65ddc1b34c0782e24dff0cbd7eae4507
syoung-smallwisdom <shannon.young@sagebase.org>
2019-03-07 22:44:25 -0800

    Move to using generic instructions for the MotorControl instruction

--- MotorControl/ViewControllers/MCTCountdownStepViewController.swift
+++ MotorControl/ViewControllers/MCTCountdownStepViewController.swift
@@ -33,21 +33,11 @@
 
 import Foundation
 
-extension MCTCountdownStepViewController : MCTInternalStepController {
-}
-
 open class MCTCountdownStepViewController : RSDCountdownStepViewController, MCTHandStepController {
     
     /// Retuns the imageView, in this case the image from the navigationHeader.
     public var imageView: UIImageView? {
         return self.navigationHeader?.imageView ?? self.navigationBody?.imageView
-    }
-    
-    /// Override show learn more to insert the first run answer result.
-    override open func showLearnMore() {
-        // Update the first run to show the full instructions.
-        setIsFirstRunResult(true)
-        super.showLearnMore()
     }
     
     /// Override viewWillAppear to also set the unitLabel text.
